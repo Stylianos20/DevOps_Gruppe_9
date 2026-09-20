3fd30886cbc157b5ff93a9a9987e357c23c3e952
Mahmut Ural <mahmut.ural@mnd.thm.de>
2026-09-20 21:39:19 +0200

    Korrektur letzter Commit

--- src/main/java/com/example/Spring_Boot_App/ApiController.java
+++ src/main/java/com/example/Spring_Boot_App/ApiController.java
@@ -1,4 +1,4 @@
-package com.example.devops;
+package com.example.Spring_Boot_App;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
